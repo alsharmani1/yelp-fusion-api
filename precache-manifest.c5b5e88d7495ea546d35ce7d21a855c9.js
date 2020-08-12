5a4e231fe5146abf7c2ea3c8ceb6cf96ee078d9e
Abdallah Alsharmani <alsharmani1@users.noreply.github.com>
2020-08-12 13:29:19 -0400

    Updates

---
+++ precache-manifest.c5b5e88d7495ea546d35ce7d21a855c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3137eeae0cfc1e7841e8394c030ad9",
+    "url": "/yelp-fusion-api/index.html"
+  },
+  {
+    "revision": "3dab9cfb058c111bc3fd",
+    "url": "/yelp-fusion-api/static/js/2.f6ec210c.chunk.js"
+  },
+  {
+    "revision": "d0d0eff58ab8cd656cd1",
+    "url": "/yelp-fusion-api/static/js/main.cb25d122.chunk.js"
+  },
+  {
+    "revision": "bbad12afa1d2157758e5",
+    "url": "/yelp-fusion-api/static/js/runtime~main.60cb6907.js"
+  }
+]);
